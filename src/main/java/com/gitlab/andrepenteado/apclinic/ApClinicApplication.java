b076e9b3d279cfe17077faf461bc5b706ab62172
Andre Penteado <andre.penteado@unesp.br>
2020-02-14 11:18:22 -0300

    Convertido de fat jar para war

--- src/main/java/com/gitlab/andrepenteado/apclinic/ApClinicApplication.java
+++ src/main/java/com/gitlab/andrepenteado/apclinic/ApClinicApplication.java
@@ -2,6 +2,7 @@ package com.gitlab.andrepenteado.apclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
-public class ApClinicApplication {
+public class ApClinicApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApClinicApplication.class, args);
